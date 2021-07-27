44b30917c571cfb9c73a9cefdbf6fd3c169a30c0
Jainam Oswal <76057348+jainamoswal@users.noreply.github.com>
2021-07-27 17:30:29 +0530

    Added few features ✨

--- ReadMe.md
+++ ReadMe.md
@@ -6,41 +6,70 @@
 [![Contributors](https://img.shields.io/github/contributors/jainamoswal/BotStatus?style=for-the-badge&color=green)](https://github.com/jainamoswal/BotStatus)
 
 
+
 ## BotStatus ~ 
 
-A simple & short repository to show your bot's status in your GitHub README.md file as well as in you channel. 
+A simple, short, mostly customisable GitHub Action to show your bot's status in your GitHub MarkDown file as well as in your Telegram. 
 
 ---
 ### How to use this ?
-
 - **Star** this repo. ⭐
 - Go to the repository where you want to display the status of the bots. 🤖
 - Go to environment variables ([Settings ⇢ Secrets ⇢ New Repository Secret.](https://docs.github.com/en/actions/reference/encrypted-secrets)) 🚶
 - Fill all Environment variables there. 🤭
 - Copy [this](./example.yml) snippet in `./github/workflows/main.yml` in your repository. 📁
-- The workflow will automatically run at interval of 3 hours. 🏃
-  
-_⚠️ You'll need a bots.json file and list all bots in the JSON format.
-It can be raw link from `gist.github.com` or directly by the file itself like `https://github.com/<blabla>/raw/<branchname>/<filename>`. But it should point to the raw source._ 
+- The workflow will automatically run at interval of 3 hours. 🏃 
 
+**⚠️ You'll need a valid JSON with all your bots with some additional information. Example is given in example.json, But the link shuld point to raw data like [here](https://gist.githubusercontent.com/jainamoswal/cc331a3d2a4169ab76885c5a1e076d68/raw/579dc4eabce06bc8d4e4e2192449bf1bf53a8193/bots.json). You can use [npoint.io](https://npoint.io), [gist.github.com](https://gist.github.com), [pastebin.com](https://pastebin.com), [JSONKeeper.com](https://jsonkeeper.com) etc...**
+
+**You can also use `${{ steps.bot-status.outputs.STATUS }}` for using the JSON data (containing the name of bot, username and status) wherever you want to use them.**
+
+> 3rd and 4th steps are not mandatory, It's just to keep your secrets completely secret and not let anyone peep into them. You can also define them directly in `.yml` file.
+
+
+---
 
 <details>
   <summary><b>🤫&nbsp;Environment variables</b></summary>
   <br/>
 
-| 🔒 Secret 🔒 | ✍️ Description ✍️ |
-| :-: | :-: |
-| `API_HASH` | Get it from [my.telegram.org](https://my.telegram.org) |
-| `APP_ID` | Get it from [my.telegram.org](https://my.telegram.org) |
-| `CHANNEL_ID` | Channel ID eg. -10010254xxxxx |
-| `MESSAGE_ID` | Message ID of the message to edit. |
-| `SESSION` | [![Run on Repl.it](https://replit.com/badge/github/jainamoswal/SessionString)](https://replit.com/@jainamoswal/SessionString) |
-| `BOTS` | Raw link of JSON file of bots. [example](./example.json) |
-  
-<b>Optional variables ~ </b>
-- `FILE_NAME` : Name of the MarkDown file, eg. `README.md`. This is case sensitive. Defaults to `README.md`.
+<b>⌲ These are mandatory variables and should be kept secret. (use [Environment variables](https://docs.github.com/en/actions/reference/encrypted-secrets))</b>
+
+| 🔒 Secret 🔒 | 🏷 Description 🏷 | ⚙️ Default ⚙️ | 📇 Example 📇 |
+| :-: | :-: | :-: | :-: |
+| `API_HASH` | Get it from [my.telegram.org](https://my.telegram.org) | `None` | `782xxxx` |
+| `APP_ID` | Get it from [my.telegram.org](https://my.telegram.org) | `None` | `a1bbfb767fd59812bxxxxxxxxxxxxxxx` |
+| `IDS` | IDs of the Messgage followed with `chat id` | `None` | `-100153418xxxx:3 -100225478xxxx:16` |
+| `SESSION` | [![Run on Repl.it](https://replit.com/badge/github/jainamoswal/SessionString)](https://replit.com/@jainamoswal/SessionString) | `None` | `xxxxxxxxxxxxxxxx.....` |
+| `BOTS` | Raw link of JSON file of bots. | [🔗 Link 🔗](https://gist.githubusercontent.com/jainamoswal/cc331a3d2a4169ab76885c5a1e076d68/raw/579dc4eabce06bc8d4e4e2192449bf1bf53a8193/bots.json) | [JSON format](/example.json) ║ [Raw link](https://gist.githubusercontent.com/jainamoswal/cc331a3d2a4169ab76885c5a1e076d68/raw/579dc4eabce06bc8d4e4e2192449bf1bf53a8193/bots.json) |
+
+
+**Format of chat IDs » `chat id`:`message id` 
+Eg, `-100123456xxx:1x` where `100123456xxx` is `chat id` and `1x` is `message id`.**
+
+<details><summary><b>Optional variables</b></summary>
+<br>
+
+<b>⌲ These are optional values and can be omitted. Default values will be used. (can use directly in `.yml` instead of messing Environment variables.)</b> 
+| 🧾 Values 🧾 | 🏷 Description 🏷 | ⚙️ Default ⚙️ | 📇 Example 📇 |
+| :-: | :-: | :-: | :-: |
+| `FILE_NAME` | Name of the file to edit.  | `README.md` | `STATUS.md` |
+| `EDIT_IN_REPO` | `True` for editing status in repository, `False` for else. | `True` | `True` |
+| `EDIT_IN_TELEGRAM` | True for editing status in Telegram. | `True` | `True` |
+| `START_MESSAGE` | Message to be shown in starting of the Text in Telegram. | _Read [main.py](./main.py)_ | _Read [main.py](./main.py)_ |
+| `END_MESSAGE` | Message to be shown in ending of the Text in Telegram. | _Read [main.py](./main.py)_ | _Read [main.py](./main.py)_ |
+| `COMMIT_MESSAGE` | Message while commit. | `✨ auto-updated bot status. ✨` | `Updated bot status [Bot]` |
+| `BULLET` | The bullet use to separate bots in Telegram. | `◪` | `◍` |
+| `TIME_ZONE` | The Time zone of your locale. | `Asia/Kolkata` | `Europe/London` |
+| `TIME_FORMAT` | Format of your time to be shown everwhere. | `%H:%M %d/%m` | `%Y-%m-%d %H:%M:%S` |
+| `UP_GITHUB` | Emoji or Text when the status is up. (on GitHub MarkDown file) | `✔️` | `✅` |
+| `DOWN_GITHUB` | Emoji or Text when the status is down. (on GitHub MarkDown file) | `❌` | `❎` |
+| `UP_TELEGRAM` | Emoji or Text when the status is up. (in Telegram chat) | `🚀` | `☑️` |
+| `DOWN_TELEGRAM` | Emoji or Text when the status is down. (in Telegram chat) | `❌` | `💤` |
+
 </details>
 
+</details>
 
 <details>
   <summary><b>:v: &nbsp;Support me</b></summary>
@@ -66,9 +95,10 @@ It can be raw link from `gist.github.com` or directly by the file itself like `h
   <summary><b>💻&nbsp;Credits</b></summary>
   <br/>
 
-- [xditya](https://github.com/xditya) for inspiration from [his repo.](https://github.com/xditya/BotStatus)
-- [Lonami](https://github.com/LonamiWebs) for telethon.
-- [Google](https://google.com) and [Telethon docs](https://docs.telethon.dev/en/latest)  😅
+- [Aditya](https://github.com/xditya) for inspiration from [his repo](https://github.com/xditya/BotStatus).
+- [Vaibhav-08](https://github.com/vaibhav-08) for making me aware of typos, suggesting features and a lot that should deserve ["Thanks"](https://telegra.ph/file/5a50245e97d7bee54f513.gif).
+- [Lonami](https://github.com/LonamiWebs) for Telethon.
+- [Google](https://google.com) and [Telethon docs](https://docs.telethon.dev/en/latest).
 </details>
 
 <hr/>
--- main.py
+++ main.py
@@ -1,41 +1,76 @@
+#!/usr/bin/env python
 # This file is part of https://github.com/jainamoswal/BotStatus.
 # Usage covered in < GPL-3.0 License >
 # Jainam Oswal. <jainam.me> 
 
 # import modules
-import os, logging, asyncio, re, github, requests, platform
+import os
+import re
+import json
+import github
+import logging
+import telethon
+import requests
+import platform
 from pytz import timezone 
 from datetime import datetime
-import telethon
+from telethon.errors import MessageIdInvalidError, MessageAuthorRequiredError
 
 print(':::::::::::::::::::::: ⏲️ Action started successfully ⏲️ ::::::::::::::::::::::')
 print()
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.CRITICAL)
 
-# define configs
-bots = requests.get(os.getenv('BOTS')).json() # the raw link of the gist file having config data in JSON format
-api_id = os.getenv('APP_ID') # API ID from my.telegram.org
-api_hash = os.getenv('API_HASH') # APP hash from my.telegram.org
-session = os.getenv('SESSION') # session string via telethon
-client = telethon.TelegramClient(telethon.sessions.StringSession(session), api_id, api_hash) # none of your bussiness 
-file_name = 'README.md' if os.getenv('FILE_NAME') is None else os.getenv('FILE_NAME')  # filename is case sensitive
+# define configs & default configs and blabla...
+# Required :- 
+bots = requests.get(os.getenv('BOTS', 'https://bit.ly/3zzCVJE')).json() # the raw link of the gist file having config data in JSON format. Use mine incase of None provided. 😂 
+api_id = os.getenv('APP_ID', None) # API ID from my.telegram.org
+api_hash = os.getenv('API_HASH', None) # APP hash from my.telegram.org
+session = os.getenv('SESSION', None) # session string via telethon.
+client = telethon.TelegramClient(telethon.sessions.StringSession(session), api_id, api_hash) # none of your business.
+all_mixed_ids = os.getenv('IDS', None) # All Chat IDs along with message IDs (Telegram) to edit in format chat_id:message_id. In case of many, separate them with spaces. Eg, -100123456xxx:8x 123456xx:2xx
+
+# Optional :-
+file_name = os.getenv('FILE_NAME', 'README.md') # filename is case sensitive.
+edit_in_repo = os.getenv('EDIT_IN_REPO', 'true')  # If you want to edit status in GitHub Repo, set it to True else False.
+edit_in_telegram = os.getenv('EDIT_IN_TELEGRAM', 'true') # If you want to edit status in Telegram, set it to True else False
+start_text = "Live status of my bots goes following ~" # default for start_message.
+start_message = os.getenv('START_MESSAGE', start_text) # text before the status to show.
+end_text = "**Bots status are auto-updated every 3 hours at random frequency.**" # default for end_message.
+end_message = os.getenv('END_MESSAGE', end_text) # text after the status to show.
+commit_message = os.getenv('COMMIT_MESSAGE', '✨ auto-updated bot status. ✨') # commit message at status update. Btw, stars looks cool.
+bullet = os.getenv('BULLET', '◪') # if you want to get custom bullets in Telegram.
+time_zone = os.getenv('TIME_ZONE', 'Asia/Kolkata') # ISD. You can choose different as per your location.
+time_format = os.getenv('TIME_FORMAT', '%H:%M %d/%m') # Time format, defaults to Hrs:minutes Day/Month. Eg, 9:41 12/9
+current_time = datetime.now(timezone(time_zone)).strftime(time_format) # Time when the script runs.
+up_github = os.getenv('UP_GITHUB', '✔️') # Custom Icon when Bot is up to show in GitHub MarkDown file.
+down_github = os.getenv('DOWN_GITHUB', '❌') # Custom Icon when Bot is down to show in GitHub MarkDown file.
+up_telegram = os.getenv('UP_TELEGRAM', '🚀') # Custom Icon when Bot is up to show in Telegram.
+down_telegram = os.getenv('DOWN_TELEGRAM', '❌') # Custom Icon when Bot is down to show in Telegram.
 
 # print some information
 def display():
-  print()
-  print(f"◻️ Telethon version : {telethon.__version__}") # telethon version
-  print(f"◻️ Platform : {platform.system()}") # system
-  print(f"◻️ Platform release : {platform.release()}") # system version
-  print(f"◻️ OS version : {platform.version().split()[0]}") # OS version
-  print()
+    print()
+    print(f"◻️ Telethon version : {telethon.__version__}") # telethon version
+    print(f"◻️ Platform : {platform.system()}") # system
+    print(f"◻️ Platform release : {platform.release()}") # system version
+    print(f"◻️ OS version : {platform.version().split()[0]}") # OS version
+    print()
 
-# updates in ReadMe file at github
+# returns a list of proper formatted ids to edit in a looooooop.
+def get_ids(all_mixed_ids):
+    all_chats_with_ids_mix = all_mixed_ids.split(' ')
+    all_chats = []
+    for each in all_chats_with_ids_mix:
+        all_chats.append(each.split(':'))
+    return all_chats
+
+# updates in ReadMe file at GitHub
 def updateme(old, json_data, first_match, second_match):
     new = '''\n| 🤖 Bot 🤖 | ⭐️ Status ⭐️ |\n| :-: | :-: |\n'''
     for i in json_data:
-        new += f"| [{json_data[i]['name']}](https://t.me/{i}) | {'✔️' if json_data[i]['status'] else '❌'} |\n"
-    new_string = f"\n{first_match}\n{new}\n`Updated last at ~ {datetime.now(timezone('Asia/Kolkata')).strftime('%H:%M:%S on %Y-%m-%d ')} INR` 🏳️‍🌈\n\n"
-    new_string += f"**Made with ❤️ via [BotStatus](https://github.com/jainamoswal/botstatus)**. \n{second_match}"
+        new += f"| [{json_data[i]['name']}](https://t.me/{i}) | {up_github if json_data[i]['status'] else down_github} |\n"
+    new_string = f"\n{first_match}\n{new}\n`Updated last at ~ {current_time}`\n\n"
+    new_string += f"**Made with ❤️ via [BotStatus](https://github.com/jainamoswal/botstatus)**. \n{second_match}" # self promotion is must ¯\_(ツ)_/¯
     return re.sub(f'\n{first_match}.*?{second_match}', new_string, old, flags=re.DOTALL)
 
 # fetch status of all bots listed in the raw gist file
@@ -60,22 +95,41 @@ async def main():
 # edit the message with status at telegram
 async def edit_message(data):
     async with client:
-        text = 'Live status of my bots goes following ~\n'
+        text = f'{start_message}\n' # I love f-strings and to comment every line :)
         for i in data:
-            text += f"🔅 [{data[i]['name']}](https://t.me/{i}) ~ {'🚀' if data[i]['status'] else '❌'}\n"
-        text += f"\n**Last Checked:** \n__{datetime.now(timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S')} IST__\n\n**Bots status are auto-updated every 3 hours at random frequency.**\n\n[J Projects](https://t.me/J_Projects)"
-        await client.edit_message(int(os.getenv('CHANNEL_ID')), int(os.getenv('MESSAGE_ID')), text, link_preview=False)
+            text += f"{bullet} [{data[i]['name']}](https://t.me/{i}) ~ {up_telegram if data[i]['status'] else down_telegram}\n"
+        text += f"\n**Last Checked:** \n__{current_time}__\n"
+        text += end_message
+        chats_to_edit = get_ids(all_mixed_ids)
+        for chat_id, message_id in chats_to_edit:
+            try:
+                await client.edit_message(int(chat_id), int(message_id), text, link_preview=False)
+            except MessageIdInvalidError:
+                print(f'Provided message ID ({message_id}) is invalid of chat ({chat_id}), Maybe message Id representing the Message is Deleted or was not sent before.')
+            except MessageAuthorRequiredError:
+                print(f'You don\'t have enough rights to edit this message.\n Chat : {chat_id} with Message {message_id}')
+
+# pastes the JSON output to spaceb.in/ and returns the raw link
+def PasteMe(json=None):
+    url="https://spaceb.in/api/v1/documents/"
+    json={"content": str(json), "extension": "txt"}
+    req = requests.post(url, json=json) 
+    returnMe =  url + req.json()['payload']['id'] + "/raw"
+    return returnMe
 
 # run the script via __main__ style
 if __name__ == '__main__':
     json_data = client.loop.run_until_complete(main())
+    os.system(f'echo \"output={PasteMe(json=json_data)}\" >> $GITHUB_ENV') # sets JSON data as output, so that you can use the json data for other use also.
     display()
     for each in json_data:
         print(f"🔸 {json_data[each]['name']} [@{each}] is {'🟢' if json_data[each]['status'] else '🔴'}")
-    client.loop.run_until_complete(edit_message(json_data))
-    repo = github.Github(os.getenv('GITHUB_TOKEN')).get_repo(os.getenv('GITHUB_REPOSITORY'))
-    contents = repo.get_contents(file_name) 
-    repo.update_file(file_name, "✨ auto-updated bot status. ✨", updateme(contents.decoded_content.decode(), json_data, '<start>', '<end>'), contents.sha)
+    if edit_in_telegram.lower() == "true":
+        client.loop.run_until_complete(edit_message(json_data))
+    if edit_in_repo.lower() == "true":
+        repo = github.Github(os.getenv('GITHUB_TOKEN')).get_repo(os.getenv('GITHUB_REPOSITORY'))
+        contents = repo.get_contents(file_name) 
+        repo.update_file(file_name, commit_message, updateme(contents.decoded_content.decode(), json_data, '<start>', '<end>'), contents.sha)
 
 print()
 print('::::::::::::::::::::: 🎉 Action completed successfully 🎉 :::::::::::::::::::::')
